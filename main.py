c6a92380358bc03814725859201eb39ddb9c2105
OliverK03 <oliver.kauppii@gmail.com>
2026-03-21 12:01:38 +0200

    dockeri pistää suuttumaan

--- main.py
+++ main.py
@@ -33,7 +33,6 @@ if __name__ == '__main__':
 
     # Messages
     app.add_handler(MessageHandler(filters.TEXT, handle_message))
-    app.add_handler(MessageHandler(filters.TEXT, handle_response))
 
     # Errors
     app.add_error_handler(error)
